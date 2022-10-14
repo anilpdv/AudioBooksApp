f5c9556540088f33928fb1728662a27bcdf0c45d
anilpdv <pdvanil007@gmail.com>
2022-10-14 16:45:57 +0530

    Initial Commit

---
+++ AudioBooksApp/AudioBooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBooksAppApp.swift
+//  AudioBooksApp
+//
+//  Created by anilpdv on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AudioBooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
